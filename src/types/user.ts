e88a53316b17daaa21cd1d4ed9628b6bec20fd1d
Kirill <kirill_1131@mail.ru>
2022-08-13 17:08:22 +0300

    Updates

--- src/types/user.ts
+++
@@ -1,9 +0,0 @@
-export type UserType = {
-  id: string;
-  bdate: string;
-  photo_100: string;
-  photo_200: string;
-  photo_max_orig: string;
-  first_name: string;
-  last_name: string;
-};
--- src/utils/helpers.ts
+++
@@ -1,9 +0,0 @@
-import {ChartItemDataType} from "../types/mock-data";
-
-export const getSum = (data: ChartItemDataType[]): number => {
-  return data
-    .map((el: ChartItemDataType) => el.value)
-    .reduce(function (a: number, b: number) {
-      return a + b;
-    });
-};
